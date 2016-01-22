8e473069a5f38fe6d513c064eeab5f97a5017131
Md. Rafiqul Islam Rumon <rumon2u@gmail.com>
2016-01-22 18:08:23 +0600

    new files aded

---
+++ config.rb
@@ -0,0 +1,18 @@
+require 'susy'
+require 'compass/import-once/activate'
+require 'breakpoint'
+# Require any additional compass plugins here.
+
+
+# Set this to the root of your project when deployed:
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "sass"
+css_dir = "css"
+images_dir = "img"
+font_dir = "fonts"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :sass
+output_style = :expanded
+relative_assets = true
